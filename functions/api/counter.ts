0d7d071e9046dc4f44f5aec15d7f0d5247a95ab1
Joe Johnston <joe@simple10.com>
2022-05-07 16:59:27 -0700

    Add functions test

---
+++ functions/api/counter.ts
@@ -0,0 +1,31 @@
+// const endpoint = 'REPLACE_UPSTASH_REST_ENDPOINT'
+// const token = 'REPLACE_UPSTASH_REST_TOKEN'
+
+// async function recordRequest(request) {
+//   let d = new Date();
+//   let datestr = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
+//   let data = [["url", request.url], ...request.headers]
+//   let url = endpoint + '/lpush/' + datestr
+//   const init = {
+//     body: JSON.stringify(data),
+//     method: "POST",
+//     headers: {
+//       "Authorization": "Bearer " + token
+//     },
+//   }
+//   return await fetch(url, init);
+// }
+
+export async function onRequestGet({ params }) {
+  const res = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`)
+  const data = await res.json()
+  const info = JSON.stringify(data, null, 2)
+  return new Response(info)
+}
+
+export async function onRequestPost(context) {
+  // ...
+}
+
+export const onRequestPut = onRequestPost
+
